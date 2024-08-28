9ca9a7a6658feb86d738cd654d037c2ccc6e58b3
Superior_P <superior.prv@gmail.com>
2024-08-28 09:53:23 +0500

    third commit

--- s2/api.js
+++ s2/api.js
@@ -3,6 +3,51 @@ import { Display,DisplayBag } from "./dom.js";
 const API = "http://localhost:3000/data";
 const BagAPI = 'http://localhost:3000/bag';
 
+const searchInput = document.querySelector('.searchInput');
+const selectCompany = document.querySelector('.selectCompany');
+const priceRange = document.querySelector('.priceRange');
+const priceValue = document.querySelector('.priceValue');
+
+let atr1='',atr2='',atr3=500;
+
+searchInput.oninput=(e)=>{
+    atr1 = e.target.value.trim();
+    Sorting();
+}
+
+selectCompany.oninput=(e)=>{
+    if(e.target.value == 'All'){
+        atr2='';
+        Sorting();
+    }
+    else{
+        atr2 = e.target.value;
+        Sorting();
+    }
+}
+
+priceRange.oninput=(e)=>{
+    priceValue.innerHTML = 'Price: $'+e.target.value;
+    atr3 = e.target.value;
+    Sorting();
+}
+
+async function Sorting() {
+    try {
+        const {data} = await axios.get(API);
+        let data1=[];
+        data.forEach((e,i) => {
+            if(e.name.includes(atr1) && e.manufacturer.includes(atr2) && Number(e.price)<=atr3) data1.push(e);
+        });
+        Display(data1)
+        
+    } catch (error) {
+        console.error(error);
+        
+    }
+    
+}
+
 async function GetData() {
     try {
         const {data} = await axios.get(API);
